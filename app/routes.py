8419be1ce38b77f3b3b526a7fd8a4c46f05edddc
sschwein <thatguysch@gmail.com>
2019-02-16 13:19:51 -0800

    getting flake8 working

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,14 @@
 # from app import app
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
-)
+from flask import Blueprint
 
-bp = Blueprint('blog', __name__)
+bp = Blueprint("blog", __name__)
+
 
 @bp.route("/")
 def home():
     # app.logger.info("test log")
     return "Hello, World!"
+
 
 @bp.route("/test")
 def test():
