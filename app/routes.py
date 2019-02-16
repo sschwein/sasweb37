d529380f9b6ae1c16a1daa7405194a0bae3c0432
sschwein <thatguysch@gmail.com>
2019-02-16 14:35:43 -0800

    app engine deploy test

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ bp = Blueprint("blog", __name__)
 @bp.route("/")
 def home():
     # app.logger.info("test log")
-    return "Hello, World!"
+    return "Hello, World Cloud Builder!"
 
 
 @bp.route("/test")
